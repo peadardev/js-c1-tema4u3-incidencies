8a3dcd59a8dc424368c2f02b6afd75c57b91df6f
Pere GT <peadar.dev@gmail.com>
2026-05-28 18:41:50 +0200

    incidencies

--- src/components/FormulariIncidencies.vue
+++ src/components/FormulariIncidencies.vue
@@ -3,9 +3,22 @@ import { useForm } from 'vee-validate';
 import * as yup from 'yup';
 
 const schema = yup.object({
-  subject: yup.string().required(),
+  subject: yup.string().required().min(5).max(100),
   email: yup.string().required().email(),
-  category: yup.string().required().min(3).max(100),
+  category: yup.string().required('Selecciona una categoria'),
+
+  softwareVersion: yup.number().when('category', {
+    is: 'categTech',
+    then: (schema) => schema.required().typeError('La versió ha de ser un número').min(1),
+    otherwise: () => yup.mixed().strip(),
+  }),
+
+  invoiceId: yup.string().when('category', {
+    is: 'categInvoice',
+    then: (schema) => schema.required().length(6),
+    otherwise: () => yup.mixed().strip(),
+  }),
+
   description: yup.string().required().min(20),
 });
 
@@ -13,6 +26,8 @@ const initialValues = {
   subject: '',
   email: '',
   category: '',
+  softwareVersion: '',
+  invoiceId: '',
   description: '',
 };
 
@@ -24,10 +39,28 @@ const { values, errors, meta, defineField } = useForm({
 const [subject, subjectAttrs] = defineField('subject');
 const [email, emailAttrs] = defineField('email');
 const [category, categoryAttrs] = defineField('category');
+const [softwareVersion, softwareVersionAttrs] = defineField('softwareVersion');
+const [invoiceId, invoiceIdAttrs] = defineField('invoiceId');
 const [description, descriptionAttrs] = defineField('description');
 
 function onSubmit() {
-  console.log('INCIDENCIA:', JSON.stringify(values));
+  console.log(
+    'INCIDENCIA:',
+    JSON.stringify({
+      subject: values.subject,
+      email: values.email,
+      category: values.category,
+      description: values.description,
+      details: {
+        ...(values.category === 'categTech' && {
+          softwareVersion: values.softwareVersion,
+        }),
+        ...(values.category === 'categInvoice' && {
+          invoiceId: values.invoiceId,
+        }),
+      },
+    })
+  );
 }
 </script>
 
@@ -63,17 +96,54 @@ function onSubmit() {
 
     <!-- Categoria -->
     <label for="category">Categoria de la incidència</label>
-    <input
+    <select
       id="category"
-      type="text"
       v-model="category"
       v-bind="categoryAttrs"
       :class="{ invalid: !!errors.category }"
       :aria-invalid="!!errors.category"
       aria-describedby="category-error"
-      placeholder="(categoria)"
-    />
+    >
+      <option value="">(Selecciona una categoria)</option>
+      <option value="categTech">Error tècnic</option>
+      <option value="categInvoice">Facturació</option>
+      <option value="categQuery">Consulta</option>
+    </select>
     <p v-if="errors.category" id="category-error" class="error">{{ errors.category }}</p>
+
+    <!-- Categoria: Tecnic: Versio -->
+    <label v-if="category === 'categTech'" for="softwareVersion">Versió de software</label>
+    <div v-if="category === 'categTech'">
+      <input
+        id="softwareVersion"
+        type="text"
+        v-model="softwareVersion"
+        v-bind="softwareVersionAttrs"
+        :class="{ invalid: !!errors.softwareVersion }"
+        :aria-invalid="!!errors.softwareVersion"
+        aria-describedby="softwareVersion-error"
+        placeholder="(num. versió)"
+      />
+    </div>
+    <p v-if="errors.softwareVersion" id="softwareVersion-error" class="error">
+      {{ errors.softwareVersion }}
+    </p>
+
+    <!-- Categoria: Facturacio: Num factura -->
+    <label v-if="category === 'categInvoice'" for="invoiceId">Identificador de factura</label>
+    <div v-if="category === 'categInvoice'">
+      <input
+        id="invoiceId"
+        type="text"
+        v-model="invoiceId"
+        v-bind="invoiceIdAttrs"
+        :class="{ invalid: !!errors.invoiceId }"
+        :aria-invalid="!!errors.invoiceId"
+        aria-describedby="numfactura-error"
+        placeholder="(id. factura)"
+      />
+    </div>
+    <p v-if="errors.invoiceId" id="numfactura-error" class="error">{{ errors.invoiceId }}</p>
 
     <!-- Descripció -->
     <label for="description">Descripció detallada</label>
--- src/config/yup.ts
+++ src/config/yup.ts
@@ -8,5 +8,9 @@ yup.setLocale({
     email: 'El format de correu no és correcte',
     min: ({ min }) => `Ha de tenir al menys ${min} caracters`,
     max: ({ max }) => `Ha de tenir com a màxim ${max} caracters`,
+    length: ({ length }) => `Ha de tenir exactament ${length} caracters`,
+  },
+  number: {
+    min: ({ min }) => `Ha de ser un número >= ${min}`,
   },
 });
